b78b3a0760a3f01475aefcd8dd8dd68f5b560d3b
Andrei Savenko <contact@startspace.ru>
2015-07-05 18:45:59 +0300

    Commit 3

---
+++ src/Chapter2/Echo.java
@@ -0,0 +1,18 @@
+/*
+   Try This 2-1
+
+   Compute the distance to a lightening
+   strke whose sound takes 7.2 seconds
+   to reach you.
+*/
+class Echo {
+    public static void main(String args[]) {
+        double dist;
+
+        dist = 7.2 * 1100 / 2;
+
+        System.out.println("The distance to rock is " + dist +
+                " feet away.");
+
+    }
+}
---
+++ src/Chapter2/NestVar.java
@@ -0,0 +1,20 @@
+/*
+   This program attempts to declared a variable
+   in an inner scope with the same name as one
+   defined in an outer scope.
+
+   *** This program will not compile. ***
+*/
+class NestVar {
+    public static void main(String args[]) {
+        int count;
+
+        for(count = 0; count < 10; count = count+1) {
+            System.out.println("This is count: " + count);
+
+            int count1; // illegal!!!
+            for(count1 = 0; count1 < 2; count1++)
+                System.out.println("This program is in error!");
+        }
+    }
+}
